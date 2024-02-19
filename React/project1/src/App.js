ea3368a3b9ddc804565db94bdc99c9e42dc5d8b0
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-02-19 16:19:57 +0530

    React Class 19/2

---
+++ React/project1/src/App.js
@@ -0,0 +1,36 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    
+    [
+      <h1> Welcome To React </h1>,
+      <p> The p tag is working. </p>,
+      <p> xyz added instead of div. </p>,
+      <> Empty added instead of xyz. </>,
+      <m> [] addedd instead of empty. </m>
+    ]
+  );
+}
+
+export default App;
+
+//['Apple',''Mango','Banana','Orange']
