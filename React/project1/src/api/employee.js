662197f0820c92d4dd18aa0e3103079d78058ee1
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-03-07 20:08:16 +0530

    React 07/03

--- React/project1/src/api/employee.js
+++ React/project1/src/api/employee.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 const Employee = () => {
   let [allemp, updateEmpList] = useState([]);
+  
 
   const getEmp = () => {
     fetch("http://localhost:1234/emplist")
@@ -18,84 +19,117 @@ const Employee = () => {
   }, []);
 
 
-//Save Employee
-let[empname, pickName] = useState("");
-let[empcity, pickCity] = useState("");
-let[empsalary, pickSalary] = useState("");
-let[empskill, pickSkill] = useState("");
+  //Save Employee
+  let [empname, pickName] = useState("");
+  let [empcity, pickCity] = useState("");
+  let [empsalary, pickSalary] = useState("");
+  let [empskill, pickSkill] = useState("");
 
-
-const save = () => {
-    let url = "http://localhost:1234/emplist";
-    let newEmp = {
+  const save = () => {
+    if (empid == "") {
+      let url = "http://localhost:1234/emplist";
+      let newEmp = {
         name: empname,
         city: empcity,
         salary: empsalary,
-        skill: empskill
-    }
-    let postData = {
-        headers:{'Content-Type':'application/json'},
-        method: 'post',
-        body: JSON.stringify(newEmp)
-    }
+        skill: empskill,
+      };
+      let postData = {
+        headers: { "Content-Type": "application/json" },
+        method: "post",
+        body: JSON.stringify(newEmp),
+      };
 
+      fetch(url, postData)
+        .then((response) => response.json())
+        .then((empinfo) => {
+          getEmp(); //callback to reload the list
+          pickName("");
+          pickCity("");
+          pickSalary("");
+          pickSkill("");
+        });
+    }else{
+      let url = "http://localhost:1234/emplist/"+empid;
+      let newEmp = {
+        name: empname,
+        city: empcity,
+        salary: empsalary,
+        skill: empskill,
+      };
+      let postData = {
+        headers: { "Content-Type": "application/json" },
+        method: "put",
+        body: JSON.stringify(newEmp),
+      };
+
+      fetch(url, postData)
+        .then((response) => response.json())
+        .then((empinfo) => {
+          getEmp(); //callback to reload the list
+          pickName("");
+          pickCity("");
+          pickSalary("");
+          pickSkill("");
+          updateId("");
+        });
+    }
+  };
+
+  //Edit Employee
+  let [empid, updateId] = useState("");
+  const edit = (empdata) => {
+    updateId(empdata.id);
+
+    pickName(empdata.name);
+    pickCity(empdata.city);
+    pickSalary(empdata.salary);
+    pickSkill(empdata.skill);
+  };
+
+  //Delete Employee
+  const deleteEmp = (empid) => {
+    let url = "http://localhost:1234/emplist/" + empid;
+    let postData = { method: "delete" };
     fetch(url, postData)
-    .then(response=>response.json())
-    .then(empinfo=>{
-        getEmp(); //callback to reload the list
-        pickName("");
-        pickCity("");
-        pickSalary("");
-        pickSkill("");
-    })
-}
+      .then((response) => response.json())
+      .then((empDelete) => {
+        getEmp();
+      });
+  };
 
-//Edit Employee
-const edit = () => {
-
-}
-
-//Delete Employee
-const deleteEmp = (index) => {
-    let url = "http://localhost:1234/emplist"
-
-    allemp.splice(index,1);
-    updateEmpList([...allemp]);
-}
-
-//Search Employee
-let[keyword, pickKeyword] = useState("")
+  //Search Employee
+  let [keyword, pickKeyword] = useState("");
 
 
   return (
     <div className="contianer">
       <h1>Manage Employee</h1>
 
-
-      <p align='center'> Enter Employee Details! </p>
+      <p align="center"> Enter Employee Details! </p>
       <div id="inputarea">
         <input
           type="text"
           placeholder="Enter Name"
-          onChange={(obj)=>pickName(obj.target.value)}
+          onChange={(obj) => pickName(obj.target.value)}
           value={empname}
         />
         <input
           type="text"
           placeholder="Enter City"
-          onChange={(obj)=>pickCity(obj.target.value)}
+          onChange={(obj) => pickCity(obj.target.value)}
           value={empcity}
         />
         <input
           type="text"
           placeholder="Enter Salary"
-          onChange={(obj)=>pickSalary(obj.target.value)}
+          onChange={(obj) => pickSalary(obj.target.value)}
           value={empsalary}
         />
         <input
           type="text"
           placeholder="Enter Skills"
-          onChange={(obj)=>pickSkill(obj.target.value)}
+          onChange={(obj) => pickSkill(obj.target.value)}
           value={empskill}
         />
         <button onClick={save}>Save Employee</button>
@@ -111,8 +145,6 @@ let[keyword, pickKeyword] = useState("")
         />
       </p>
 
-
-
       <table align="center">
         <thead>
           <tr>
@@ -127,18 +159,24 @@ let[keyword, pickKeyword] = useState("")
         </thead>
         <tbody>
           {allemp.map((emp, index) => {
-            if((emp.name.toLowerCase()).match(keyword.toLowerCase()))
-            return (
-              <tr key={index}>
-                <td>{emp.id}</td>
-                <td>{emp.name}</td>
-                <td>{emp.city}</td>
-                <td>{emp.salary}</td>
-                <td>{emp.skill}</td>
-                <td><button onClick={edit}>Edit</button></td>
-                <td><button onClick={deleteEmp.bind(this, index)}>Delete</button></td>
-              </tr>
-            );
+            if (emp.name.toLowerCase().match(keyword.toLowerCase()))
+              return (
+                <tr key={index}>
+                  <td>{emp.id}</td>
+                  <td>{emp.name}</td>
+                  <td>{emp.city}</td>
+                  <td>{emp.salary}</td>
+                  <td>{emp.skill}</td>
+                  <td>
+                    <button onClick={edit.bind(this, emp)}>Edit</button>
+                  </td>
+                  <td>
+                    <button onClick={deleteEmp.bind(this, emp.id)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
           })}
         </tbody>
       </table>
