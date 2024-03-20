9a6be9b61a49d546151cc71e119aca0aaa7d65ae
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-03-20 20:37:35 +0530

    React 20/03

--- React/myredux/src/usereducer.js
+++ React/myredux/src/usereducer.js
@@ -1,7 +1,15 @@
 
-const Userlist = () => {
-    let data = [];
+const Userlist = (state=[], action) => {
+    let data = Object.assign([], state); // fetch old record if any else assign with []
 
+    if(action.type==="adduser"){
+        data.push(action.fullname);
+    }
+
+    if(action.type==="deluser"){
+        data.splice(action.userindex, 1);
+    }
+    
     return data;
 }
 
