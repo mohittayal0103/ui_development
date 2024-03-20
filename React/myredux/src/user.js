a540aa44459f67a85671b55f3c17bb7c6ca2c0a0
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-03-20 20:36:54 +0530

    React 20/03

--- React/myredux/src/user.js
+++ React/myredux/src/user.js
@@ -1,19 +1,55 @@
 import { useState } from "react";
+import { UseDispatch, useDispatch, useSelector } from "react-redux";
+import Userlist from "./usereducer";
 
 const MyUser = () => {
-    return(
-        <div className="container mt-4">
-            <div className="row">
-                <div className="col-lg-12 text-center">
-                    <h3 className="mb-4"> Manage User </h3>
-                    <p>
-                        Enter User Name: <input type="text" className="m-3"/>
-                        <button className="btn btn-primary"> Save User </button>
-                    </p>
-                </div>
-            </div>
+  let alluser = useSelector((state) => state.Userlist);
+  let dispatch = useDispatch();
+  let [newuser, pickUser] = useState("");
+
+  const save = () => {
+    let userinfo = { type: "adduser", fullname: newuser };
+    dispatch(userinfo);
+    pickUser("");
+  };
+
+  const deluser = (index) => {
+    let userinfo = {type:"deluser", userindex: index}
+    dispatch(userinfo);
+  }
+
+  return (
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h3 className="mb-4"> Manage User: {alluser.length} </h3>
+          <p>
+            Enter User Name:{" "}
+            <input
+              type="text"
+              className="m-3"
+              onChange={(obj) => pickUser(obj.target.value)}
+              value={newuser}
+            />
+            <button className="btn btn-primary" onClick={save}>
+              {" "}
+              Save User{" "}
+            </button>
+          </p>
         </div>
-    );
-}
+      </div>
+      <div className="row mt-4">
+        {alluser.map((username, index) => {
+          return (
+            <div className="mb-4 col-lg-3" key={index}>
+              <p> {username} </p>
+              <button onClick={deluser.bind(this, index)}>Delete</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default MyUser;
