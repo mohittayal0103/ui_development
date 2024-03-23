b0428e5b03dc711ddb4d07f2047836de8a8a28af
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-03-23 20:01:10 +0530

    Redux 23/03

--- React/myredux/src/productreducer.js
+++ React/myredux/src/productreducer.js
@@ -1,5 +1,13 @@
-const Productlist = () => {
-    let data = [];
+const Productlist = (state=[], action) => {
+
+    let data = Object.assign([], state);
+
+    if(action.type === "saveproduct"){
+        data.push(action.pinfo);
+    }
+    if(action.type==="deleteproduct"){
+        data.splice(action.pindex, 1);
+    }
 
     return data;
 }
