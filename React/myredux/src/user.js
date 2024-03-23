a7f835f34c0d31dae575c77d49dfc62097020ef8
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-03-24 03:46:41 +0530

    Myredux 24/03

--- React/myredux/src/user.js
+++ React/myredux/src/user.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { UseDispatch, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Userlist from "./usereducer";
 
 const MyUser = () => {
