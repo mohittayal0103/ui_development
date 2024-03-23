3439aca61aedac47b66107a52dd0ac3de77b5296
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-03-23 20:00:33 +0530

    Redux 23/03

--- React/myredux/src/dashboard.js
+++ React/myredux/src/dashboard.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 const MyDashboard = () => {
+  let alluser = useSelector((state) => state.Userlist);
+  let allproduct = useSelector(state=>state.Productlist);
   return (
     <div className="container mt-4">
       <div className="row">
@@ -8,10 +11,10 @@ const MyDashboard = () => {
           <h3> React and Redux Dashboard </h3>
         </div>
         <div className="col-lg-4">
-            <h5>Total User in Redux : 111</h5>
+            <h5>Total User in Redux : {alluser.length}</h5>
         </div>
         <div className="col-lg-4">
-            <h5>Total Product in Redux : 111</h5>
+            <h5>Total Product in Redux : {allproduct.length}</h5>
         </div>
         <div className="col-lg-4">
             <h5>Total Api Record in Redux : 111</h5>
