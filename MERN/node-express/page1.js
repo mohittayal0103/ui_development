ad28cbb1eb44b6b11c41331c065608d7c3c396e2
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-03-26 22:37:13 +0530

    node 26/03

--- MERN/node-express/page1.js
+++ MERN/node-express/page1.js
@@ -2,7 +2,7 @@ let http = require("http");
 
 http
   .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
+    res.writeHead(200, { "Content-Type":"text/html" });
 
     for (let i = 1; i <= 10; i++) {
       res.write("<p>" + i + "</p>");
