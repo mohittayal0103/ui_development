aa29956f1c62eb67893c1809f042efd25d3261a0
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-03-27 18:27:48 +0530

    3rd example

---
+++ MERN/node-express/page3.js
@@ -0,0 +1,24 @@
+let http = require("http");
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/json" });
+
+    let booklist = [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React",
+      "NodeJS",
+      "MongoDB",
+      "express",
+    ];
+
+    let jsonData = JSON.stringify(booklist); //converting array to json
+
+    res.write(jsonData);
+
+    res.end();
+
+  })
+  .listen(1234);
