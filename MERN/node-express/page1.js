7065ebb667b78b39713f5acb316dc139197fdf34
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-03-27 18:27:24 +0530

    1st example

--- MERN/node-express/page1.js
+++ MERN/node-express/page1.js
@@ -8,12 +8,6 @@ http
       res.write("<p>" + i + "</p>");
     }
     res.end();
-    // res.write("<p>dsaghskahkdsj</p>"); -> It won't print because response in ended in above line. 
+    // res.write("<p>dsaghskahkdsj</p>"); -> It won't print because response is ended in above line. 
   })
   .listen(1234);
-
-  //http://localhost:1234
-
-  //HomeWork
-  //in mern folder open cmd and after that we have to setup 2 projects npx create-react-app project1 and project2
-  //open mongoDB Website -> Products -> community edition -> scroll down-select package -> version 7.0.7 package msi download
