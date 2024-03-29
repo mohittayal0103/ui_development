a2add40aa44443627af6a1427ae6c00dfd859ed1
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-03-29 18:22:24 +0530

    node 29/03

--- MERN/project1/src/App.js
+++ MERN/project1/src/App.js
@@ -1,10 +1,36 @@
-import ManageBook from './book';
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
+
+import ManageBook from "./book";
+import ManageProduct from "./product";
 
 function App() {
   return (
-    <section>
-      <ManageBook/>
-    </section>
+    <HashRouter>
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-lg-12">
+            <h2 className="text-danger"> MERN Stack </h2>
+          </div>
+          <div className="col-lg-11 text-end">
+            <div className="btn-group">
+              <Link className="btn btn-primary" to="/book">
+                {" "}
+                Book{" "}
+              </Link>
+              <Link className="btn btn-info" to="/product">
+                {" "}
+                Product{" "}
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Routes>
+        <Route exact path="/book" element={<ManageBook />} />
+        <Route exact path="/product" element={<ManageProduct />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
