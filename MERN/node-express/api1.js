23fff1647d404d2db8d57ffc6a19067043723ce3
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-04-01 18:25:13 +0530

    node 01/04

--- MERN/node-express/api1.js
+++ MERN/node-express/api1.js
@@ -3,10 +3,15 @@ const app = express(); // creating object of express
 const cors = require("cors"); // calling cors origin library to allow data communication between 2 server
 app.use(cors()); // creating oject of cors library
 app.use(express.json()); // enable json data communication
+const fs = require("fs"); //fs is a module just used for file read-write system
+
+//--------------------------------------------------------
 
 app.get("/", function (req, res) {
   res.send("<h1> The Server is Live Now! </h1>");
 });
+
+//--------------------------------------------------------
 
 app.get("/booklist", (req, res) => {
   let booklist = [
@@ -23,6 +28,8 @@ app.get("/booklist", (req, res) => {
   res.send(jsonData);
 }); //http://localhost:2222/booklist
 
+//--------------------------------------------------------
+
 app.get("/allbook", (req, res) => {
   let allbook = [
     { name: "html", price: 3000, seller: "Uttu" },
@@ -34,32 +41,57 @@ app.get("/allbook", (req, res) => {
 
   let jsonData = JSON.stringify(allbook);
   res.send(jsonData);
-}); //http://localhost:2222/allbook 
+}); //http://localhost:2222/allbook
+
+//--------------------------------------------------------
 
 app.get("/allbook1", (req, res) => {
   let allbook1 = [
-    {name:"HTML", price:3000, seller:"Uttu", city:['bangalore','pune']},
-    {name:"CSS", price:4000, seller:"Mohit", city:['chennai','hyderabad','bangalore']},
-    {name:"React", price:5000, seller:"Mihir", city:['delhi']},
-    {name:"NodeJS", price:6000, seller:"Mrinal", city:['mumbai']},
-    {name:"MongoDB", price:7000, seller:"Manan", city:[]}
-]
+    { name: "HTML", price: 3000, seller: "Uttu", city: ["bangalore", "pune"] },
+    { name: "CSS", price: 4000, seller: "Mohit", city: ["chennai", "hyderabad", "bangalore"] },
+    { name: "React", price: 5000, seller: "Mihir", city: ["delhi"] },
+    { name: "NodeJS", price: 6000, seller: "Mrinal", city: ["mumbai"] },
+    { name: "MongoDB", price: 7000, seller: "Manan", city: [] },
+  ];
 
-let jsonData = JSON.stringify(allbook1);
-res.send(jsonData);
+  let jsonData = JSON.stringify(allbook1);
+  res.send(jsonData);
 }); //http://localhost:2222/allbook1
 
-app.get("/alldata", (req,res) => {
+//--------------------------------------------------------
+
+app.get("/alldata", (req, res) => {
   let alldata = {
-    "citylist":['Bangalore', 'Chennai', 'Kolkata', 'Delhi', 'Patna', 'Mumbai'],
-    "userlist":['Alex', 'Sumit', 'Mohit', 'Raj', 'Manoj', 'Puri'],
-    "booklist":['HTML', 'CSS', 'BootStrap', 'JavaScript', 'React', 'Node']
-  }
+    citylist: ["Bangalore", "Chennai", "Kolkata", "Delhi", "Patna", "Mumbai"],
+    userlist: ["Alex", "Sumit", "Mohit", "Raj", "Manoj", "Puri"],
+    booklist: ["HTML", "CSS", "BootStrap", "JavaScript", "React", "Node"],
+  };
 
   let jsonData = JSON.stringify(alldata);
   res.send(jsonData);
-}) //http://localhost:2222/alldata
+}); //http://localhost:2222/alldata
+
+//--------------------------------------------------------
+
+app.post("/savemsg", (req, res) => {
+  let data = req.body.mymessage.trim();
+
+  if (data == "") {
+    let jsonData = JSON.stringify({ message: "Invalid Message !" });
+    res.send(jsonData);
+  } 
+  else {
+    data = data + " - Posted on: " + new Date().toLocaleString() + "\n";
+    fs.appendFile("allmessage.txt", data, function(err, filedata){
+      let jsonData = JSON.stringify({message: "Message Received Successfully !"});
+      res.send(jsonData);
+    }); // post new content into a file through appenfile func
+  }
+}); //http://localhost:2222/savemsg 
+
+//--------------------------------------------------------
 
 app.listen(2222, function () {
   console.log("Server Started on http://localhost:2222");
 });
+ 
