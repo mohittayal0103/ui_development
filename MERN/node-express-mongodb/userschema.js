9effaed45b4c5f8773833aea382eaac0b8f513fb
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-04-03 18:40:53 +0530

    node 03/04

---
+++ MERN/node-express-mongodb/userschema.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const tableStructure = new mongoose.Schema({
+    fullname : {type:String, require:true},
+    mobile : {type:Number, required:true},
+    email : {type:String, required:true},
+    address : {type:String},
+    
+});
+
+module.exports = mongoose.model("User", tableStructure);
