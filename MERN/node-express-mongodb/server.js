63a4ba3f5d2b3090041fffd650761ea72b3157fc
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-04-10 17:54:24 +0530

    Node mongoDB 10/04

--- MERN/node-express-mongodb/server.js
+++ MERN/node-express-mongodb/server.js
@@ -1,22 +1,23 @@
-const express = require("express"); //calling express framework
-const app = express(); // creating object of express
-const cors = require("cors"); // calling cors origin library to allow data communication between 2 server
-app.use(cors()); // creating oject of cors library
-app.use(express.json()); // enable json data communication
 
-const mongoose = require("mongoose"); // calling mongoose
-mongoose.connect("mongodb://127.0.0.1:27017/company") // importing mongoose database
+const express = require("express");  // calling express framework
+const app = express();              // creating object of express
+const cors = require("cors");      // calling cors origin library to allow data communication between 2 server
+app.use(cors());                  // creating object of cors library
+app.use(express.json());         // enable json data communication
+
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/company")
 const db = mongoose.connection;
 
-//checking connection is established or not 
-db.on("error", (error)=>console.log("Error in database connection")); 
+db.on("error", (error)=>console.log("Error in database connection"));
 db.on("open", ()=>console.log("Database is Connected..."));
 
 const User = require("./userapi");
 
 app.use("/user", User); 
-// http://localhost:4444/user (get,post,put,delete)
+// http://localhost:4444/user   (get, post, put, delete)
 
 app.listen(4444, function(){
-    console.log("The server is live...")
-})
+    console.log("The server is live...");
+});
